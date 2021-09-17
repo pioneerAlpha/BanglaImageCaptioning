867b572ad10e4a12044a83b23abcb25097a9e542
Sourav Saha <70741158+srv-sh@users.noreply.github.com>
2021-09-17 21:24:43 +0600

    Add files via upload

---
+++ datasets/coco.py
@@ -0,0 +1,115 @@
+from torch.utils.data import Dataset
+import torchvision.transforms.functional as TF
+import torchvision as tv
+
+
+from PIL import Image
+import numpy as np
+import random
+import os
+
+from transformers import BertTokenizer
+
+from .utils import nested_tensor_from_tensor_list , read_json
+
+MAX_DIM = 299
+
+def under_max(image):
+    if image.mode != 'RGB':
+        image = image.convert("RGB")
+    
+    shape = np.array(image.size , dtype = np.float)
+    long_dim = max(shape)
+    scale = MAX_DIM / long_dim
+    
+    new_shape = (shape * scale).astype(int)
+    image  = image.resize(new_shape)
+    
+    return image
+
+class RandomRotation:
+    def __init__(self , angles=[0,90,180,270]):
+        self.angles = angles
+    
+    def __call__(self,x):
+        angle = random.choice(self.angles)
+        return TF.rotate(x, angle , expand =True)
+    
+train_transform = tv.transforms.Compose([
+    RandomRotation(),
+    tv.transforms.Lambda(under_max),
+    tv.transforms.ColorJitter(brightness=[0.5, 1.3], contrast=[
+                              0.8, 1.5], saturation=[0.2, 1.5]),
+    tv.transforms.RandomHorizontalFlip(),
+    tv.transforms.ToTensor(),
+    tv.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+])
+
+val_transform = tv.transforms.Compose([
+    tv.transforms.Lambda(under_max),
+    tv.transforms.ToTensor(),
+    tv.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+class CocoCaption(Dataset):
+    def __init__(self, root, ann, max_length, limit, transform=train_transform, mode='training'):
+        super().__init__()
+        self.root = root
+        self.transform = transform
+        self.annot = [(self._process(val['filename']), val['caption'][0])
+                      for val in ann]
+        self.annot1 = [(self._process(val['filename']), val['caption'][1])
+                      for val in ann] ###
+        self.annot.extend(self.annot1)###
+        if mode == 'validation':
+            self.annot = self.annot[-1830:]
+        if mode == 'training':
+            self.annot = self.annot
+
+        self.tokenizer = BertTokenizer.from_pretrained(
+            "sagorsarker/bangla-bert-base", do_lower=True)
+        self.max_length = max_length + 1
+        
+    def _process(self, image_id):
+        val = str(image_id)
+        return val
+    
+    def __len__(self):
+        return len(self.annot)
+    
+    def __getitem__(self, idx):
+        image_id, caption = self.annot[idx]
+        image = Image.open(os.path.join(self.root, image_id))
+
+        if self.transform:
+            image = self.transform(image)
+        image = nested_tensor_from_tensor_list(image.unsqueeze(0))
+
+        caption_encoded = self.tokenizer.encode_plus(
+            caption, max_length=self.max_length, pad_to_max_length=True, return_attention_mask=True, return_token_type_ids=False, truncation=True)
+
+        caption = np.array(caption_encoded['input_ids'])
+        cap_mask = (
+            1 - np.array(caption_encoded['attention_mask'])).astype(bool)
+
+        return image.tensors.squeeze(0), image.mask.squeeze(0), caption, cap_mask
+
+def build_dataset(config, mode='training'):
+    if mode == 'training':
+        train_dir = os.path.join(config.dir, 'training')
+        train_file = os.path.join(
+            config.dir, 'annotations', 'captions.json')
+        data = CocoCaption(train_dir, read_json(
+            train_file), max_length=config.max_position_embeddings, limit=config.limit, transform=train_transform, mode='training')
+        return data
+
+    elif mode == 'validation':
+        val_dir = os.path.join(config.dir, 'validation')
+        val_file = os.path.join(
+            config.dir, 'annotations', 'captions.json')
+        data = CocoCaption(val_dir, read_json(
+            val_file), max_length=config.max_position_embeddings, limit=config.limit, transform=val_transform, mode='validation')
+        return data
+
+    else:
+        raise NotImplementedError(f"{mode} not supported")
---
+++ predict.py
@@ -0,0 +1,101 @@
+import torch
+
+from transformers import BertTokenizer
+from PIL import Image
+import argparse
+import matplotlib.pyplot as plt
+from models import caption
+from datasets import coco, utils
+from configuration import Config
+import os
+
+parser = argparse.ArgumentParser(description='Image Captioning')
+parser.add_argument('--path', type=str, help='path to image')
+parser.add_argument('--checkpoint', type=str, help='checkpoint path')
+args = parser.parse_args()
+image_path = args.path
+#version = args.v
+checkpoint_path = args.checkpoint
+
+config = Config()
+
+# if version == 'v1':
+#     model = torch.hub.load('saahiluppal/catr', 'v1', pretrained=True)
+# elif version == 'v2':
+#     model = torch.hub.load('saahiluppal/catr', 'v2', pretrained=True)
+# elif version == 'v3':
+#     model = torch.hub.load('saahiluppal/catr', 'v3', pretrained=True)  
+
+# else:
+
+print("Checking for checkpoint.")
+if checkpoint_path is None:
+  raise NotImplementedError('No model to chose from!')
+else:
+  if not os.path.exists(checkpoint_path):
+    raise NotImplementedError('Give valid checkpoint path')
+  print("Found checkpoint! Loading!")
+  model,_ = caption.build_model(config)
+  print("Loading Checkpoint...")
+  checkpoint = torch.load(checkpoint_path, map_location='cuda')
+  model.load_state_dict(checkpoint['model'])
+tokenizer = BertTokenizer.from_pretrained("sagorsarker/bangla-bert-base")
+
+start_token = tokenizer.convert_tokens_to_ids(tokenizer._cls_token)
+end_token = tokenizer.convert_tokens_to_ids(tokenizer._sep_token)
+
+image = Image.open(image_path)
+image = coco.val_transform(image)
+image = image.unsqueeze(0)
+
+
+def create_caption_and_mask(start_token, max_length):
+    caption_template = torch.zeros((1, max_length), dtype=torch.long)
+    mask_template = torch.ones((1, max_length), dtype=torch.bool)
+
+    caption_template[:, 0] = start_token
+    mask_template[:, 0] = False
+
+    return caption_template, mask_template
+
+
+caption, cap_mask = create_caption_and_mask(
+    start_token, config.max_position_embeddings)
+
+
+@torch.no_grad()
+def evaluate():
+    model.eval()
+    for i in range(config.max_position_embeddings - 1):
+        predictions = model(image, caption, cap_mask)
+        predictions = predictions[:, i, :]
+        predicted_id = torch.argmax(predictions, axis=-1)
+
+        if predicted_id[0] == 102:
+            return caption
+
+        caption[:, i+1] = predicted_id[0]
+        cap_mask[:, i+1] = False
+
+    return caption
+
+
+output = evaluate()
+
+
+
+
+######################################
+#print(tokenizer.decode(output[0][2]))
+for i in range(len(output[0][1:16])):
+  print(tokenizer.decode(output[0][i]))
+
+########################################
+
+
+
+
+
+result = tokenizer.decode(output[0].tolist(), skip_special_tokens=True)
+#result = tokenizer.decode(output[0], skip_special_tokens=True)
+print("predicted caption: {}".format(result))
